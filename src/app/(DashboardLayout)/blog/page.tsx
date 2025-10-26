89b418efe20a415c232d10454e4504b87eb290bc
Will Yip <will@cloudful.io>
2025-10-26 09:46:21 -0400

    Pushed new blog components

--- src/app/(DashboardLayout)/blog/page.tsx
+++ src/app/(DashboardLayout)/blog/page.tsx
@@ -4,14 +4,14 @@ import { getAllPosts, BlogList } from "cloudful-blog"
 import path from "path";
 
 export default function BlogPage() {
-  const posts = getAllPosts(path.join(process.cwd(), "/public/blog")).sort(
+  const posts = getAllPosts(path.join(process.cwd(), "/public/blog"), true).sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   )
 
   return (
     <PageContainer title="Product Updates" description="Typing Help: Product Updates">
       <Container sx={{ mt: 0 }}>
-      <BlogList posts={posts} blogRootUrl='/blog' title='Product Updates' />
+      <BlogList posts={posts} blogRootUrl='/blog' title='Product Updates' showFullContent/>
       </Container>
     </PageContainer>
   );
