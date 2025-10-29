8a429eaae0bd8453e3d55d3d8ebe1d0d23a310c0
Will Yip <will@cloudful.io>
2025-10-28 20:49:38 -0400

    Replaced loading component with LinearProgress icon

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,18 +1,23 @@
 "use client";
-import { Box, CircularProgress } from "@mui/material";
+import { Box, LinearProgress } from "@mui/material";
 
 export default function Loading() {
   return (
     <Box
       sx={{
+        position: "fixed",     
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        height: "100vh", 
-        width: "100vw",  
+        backgroundColor: "background.default", 
+        zIndex: 1300,
       }}
     >
-      <CircularProgress />
+      <LinearProgress />
     </Box>
   );
 }
