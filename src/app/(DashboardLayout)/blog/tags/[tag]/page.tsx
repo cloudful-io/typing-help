03acdb30d0373a3caae5a861de25b5242ecaf2c4
Will Yip <will@cloudful.io>
2025-11-02 17:54:06 -0500

    Added ability to tag blog posts

--- src/app/(DashboardLayout)/blog/tags/[tag]/page.tsx
+++ src/app/(DashboardLayout)/blog/tags/[tag]/page.tsx
@@ -1,17 +1,18 @@
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
 import { Container } from "@mui/material";
-import { getAllPosts, BlogList } from "cloudful-blog"
+import { getPostsByTag, BlogTagList } from "cloudful-blog"
 import path from "path";
 
-export default function BlogPage() {
-  const posts = getAllPosts(path.join(process.cwd(), "/public/blog"), true).sort(
+export default async function BlogTagPage(props: { params: Promise<{ tag: string }> }) {
+    const { tag } = await props.params;
+    const posts = getPostsByTag(path.join(process.cwd(), "/public/blog"), tag, true).sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   )
   
   return (
     <PageContainer title="Product Updates" description="Typing Help: Product Updates">
       <Container sx={{ mt: 0 }}>
-      <BlogList posts={posts} blogRootUrl='/blog' title='Product Updates' showFullContent/>
+      <BlogTagList posts={posts} blogRootUrl='/blog' title='Product Updates' tag={tag} showFullContent/>
       </Container>
     </PageContainer>
   );
